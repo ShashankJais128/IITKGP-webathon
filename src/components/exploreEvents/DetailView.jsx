daea17265c76589104f57111bd507d1fcf3ef79a
Aditya-ranjan-16 <2005428@kiit.ac.in>
2023-01-22 12:03:24 +0530

    int

--- src/components/exploreEvents/DetailView.jsx
+++ src/components/exploreEvents/DetailView.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import rect from "../../public/Rect.png";
 import Navbar from "../header/Navbar";
 import bgex from "../../public/bgn.jpg";
@@ -10,18 +10,22 @@ import time from "../../public/time.png";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState,useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 function DetailView() {
   const [Event, setEvent] = useState([]);
+  const authCtx = useContext(AuthContext);
+  const msgref = useRef();
   const [showModal, setShowModal] = React.useState(false);
   let { id } = useParams();
   const [loading, setLoading] = useState(true);
   async function getCompetion() {
     try {
-      const resp = await axios.get("/api/competition/AllCompetition/");
+      const resp = await axios.get(`/api/competition/getCompetition/${id}`);
       const data = resp.data;
       setLoading(false);
+      console.log(data);
       setEvent(data);
     } catch (e) {
       console.log(e);
@@ -30,101 +34,109 @@ function DetailView() {
   useEffect(() => {
     getCompetion();
   }, [id]);
+  async function applySquad() {
+    const request = {
+      competitionID: id,
+      hostID: Event.host._id,
+      message: msgref.current.value,
+    };
+    try {
+      const resp = await axios.post("api/request/Add/", request, {
+        headers: { Authorization: `${authCtx.token}` },
+      });
+      const data=resp.data
+      console.log(data)
+      if(data=="Saved"){
+      setShowModal(false)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <>
       <div
         className="bg-black bg-top bg-cover h-[75vh]  "
         style={{
-          backgroundImage: ` url(${rect})`,
+          backgroundImage: loading ? ` url(${rect})` : `url(${Event.image})`,
         }}
       >
         <Navbar />
       </div>
-      <div
-        className="bg-black bg-cover bg-bottom relative h-[650px] pb-8 flex justify-center"
-        style={{
-          backgroundImage: ` url(${bgex})`,
-        }}
-      >
-        <div className="w-[90%] bg-[#28282B]  py-10 px-12 absolute -top-20">
-          <div className="flex justify-between">
-            <h1 className="text-[#ff673a] text-4xl font-bold">
-              BLANKA BOTS - KSHITIJ
-            </h1>
-            <div className="flex space-x-6">
-              <div className="flex flex-col items-center font-semibold">
-                <h1 className="text-[#ff673a] text-3xl">7</h1>
-                <h2 className="text-white">TEAM SIZE</h2>
+      {!loading && (
+        <div
+          className="bg-black bg-cover bg-bottom relative h-[650px] pb-8 flex justify-center"
+          style={{
+            backgroundImage: ` url(${bgex})`,
+          }}
+        >
+          <div className="w-[90%] bg-[#28282B]  py-10 px-12 absolute -top-20">
+            <div className="flex justify-between">
+              <h1 className="text-[#ff673a] text-4xl font-bold">
+                {Event.name}
+              </h1>
+              <div className="flex space-x-6">
+                <div className="flex flex-col items-center font-semibold">
+                  <h1 className="text-[#ff673a] text-3xl"> {Event.teamSize}</h1>
+                  <h2 className="text-white">TEAM SIZE</h2>
+                </div>
+                <div className="flex flex-col items-center font-semibold">
+                  <h1 className="text-[#ff673a] text-3xl">{Event.vac}</h1>
+                  <h2 className="text-white">VACANCY</h2>
+                </div>
               </div>
-              <div className="flex flex-col items-center font-semibold">
-                <h1 className="text-[#ff673a] text-3xl">4</h1>
-                <h2 className="text-white">VACANCY</h2>
-              </div>
             </div>
-          </div>
-          <div className="flex pb-4">
-            <img src={peop} alt="" className="w-6" />
-            <h1 className="text-white text-xl">
-              &nbsp; Posted by: <span className="underline">Rishav Singh</span>
-              &nbsp; &nbsp;
-            </h1>
-            <a href="https://www.linkedin.com" className="w-5">
-              <img src={linkd} alt="" />
-            </a>
-          </div>
-          <div className="flex">
-            <img src={cal} alt="" className="w-6" />
-            <h1 className="text-white text-xl">
-              &nbsp; Posted on: <span>23rd march 23</span>
-              &nbsp; &nbsp;
-            </h1>
-          </div>
-          <br />
-          <br />
-          <div>
-            <p className="text-justify text-2xl text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Recusandae aliquam mollitia temporibus? Ratione at enim sed
-              laudantium blanditiis consequatur, dolore non. Laboriosam nam
-              reprehenderit, a facere eaque modi sit placeat vero perferendis
-              explicabo, tenetur, ea odit sint doloribus facilis. Saepe quidem
-              sed aspernatur officiis nesciunt labore deserunt cupiditate
-              aperiam ipsum impedit vitae dolores rem ipsa, mollitia, quisquam,
-              excepturi nam dignissimos! Eaque suscipit odio dolorem enim
-              nesciunt reprehenderit sequi, reiciendis, optio inventore dolorum
-              autem mollitia. Assumenda voluptate provident voluptatum beatae
-              culpa asperiores iusto dicta maiores reprehenderit nulla quisquam
-              officiis ducimus, earum quasi sequi veniam eligendi, quae expedita
-              dolorum? Saepe, iure autem!
-            </p>
-          </div>
-          <br />
-          <br />
-          <div className="flex justify-between">
-            <div className="flex text-xl">
-              <img src={loc} alt="" className="w-6" />
-              &nbsp;
-              <h1 className="text-white">Kharagpur, Somewhere, India</h1>
+            <div className="flex pb-4">
+              <img src={peop} alt="" className="w-6" />
+              <h1 className="text-white text-xl">
+                &nbsp; Posted by:{" "}
+                <span className="underline">{Event.host.name}</span>
+                &nbsp; &nbsp;
+              </h1>
+              <a href="https://www.linkedin.com" className="w-5">
+                <img src={linkd} alt="" />
+              </a>
             </div>
-            <div className="flex text-xl">
-              <img src={time} alt="" className="w-6" />
-              &nbsp;
-              <h1 className="text-white">22:45</h1>
+            <div className="flex">
+              <img src={cal} alt="" className="w-6" />
+              <h1 className="text-white text-xl">
+                &nbsp; Posted on: <span> {Event.postDate}</span>
+                &nbsp; &nbsp;
+              </h1>
             </div>
-          </div>
-          <br />
-          <br />
-          <div className="flex justify-center">
             <br />
-            <button
-              className="bg-[#ff673a] text-white text-2xl font-semibold px-10 py-1"
-              onClick={() => setShowModal(true)}
-            >
-              Apply Now
-            </button>
+            <br />
+            <div>
+              <p className="text-justify text-2xl text-white">{Event.des}</p>
+            </div>
+            <br />
+            <br />
+            <div className="flex justify-between">
+              <div className="flex text-xl">
+                <img src={loc} alt="" className="w-6" />
+                &nbsp;
+                <h1 className="text-white">{Event.venue}</h1>
+              </div>
+              <div className="flex text-xl">
+                <img src={time} alt="" className="w-6" />
+                &nbsp;
+                <h1 className="text-white">{Event.postTime}</h1>
+              </div>
+            </div>
+            <br />
+            <br />
+            <div className="flex justify-center">
+              <br />
+              <button
+                className="bg-[#ff673a] text-white text-2xl font-semibold px-10 py-1"
+                onClick={() => setShowModal(true)}
+              >
+                Apply Now
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
 
       {/* modal */}
       {showModal ? (
@@ -142,7 +154,7 @@ function DetailView() {
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <div className="flex justify-between w-full">
                     <h3 className="text-3xl font-semibold text-[#ff673a]">
-                      BLANKA BOTS - KSHITIJ
+                      {Event.name}
                     </h3>
                     <button type="button" onClick={() => setShowModal(false)}>
                       <h1 className="text-white text-3xl">X</h1>
@@ -163,7 +175,7 @@ function DetailView() {
                   <div className="flex">
                     <img src={cal} alt="" className="w-5" />
                     <h1 className="text-white text-lg">
-                      &nbsp; Posted on: <span>23rd march 23</span>
+                      &nbsp; Posted on: <span> {Event.postDate}</span>
                       &nbsp; &nbsp;
                     </h1>
                   </div>
@@ -175,12 +187,13 @@ function DetailView() {
                     id=""
                     cols="30"
                     rows="5"
+                    ref={msgref}
                     placeholder="write your message here..."
                   ></textarea>
                 </div>
                 {/*footer*/}
                 <div className="flex justify-center items-center p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button className="bg-[#ff673a] text-white text-2xl font-semibold px-10 py-1">
+                  <button onClick={applySquad} className="bg-[#ff673a] text-white text-2xl font-semibold px-10 py-1">
                     Apply
                   </button>
                 </div>
