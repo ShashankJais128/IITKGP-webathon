f535678db1d68ad9d990a3c6a2ec1cc02a779254
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-18 22:40:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from "./components/login/Login";
 import Homemain from "./components/home/Homemain";
 import axios from "axios";
 import DetailView from "./components/exploreEvents/DetailView";
+
 axios.defaults.baseURL = "http://localhost:5000/";
 
 function App() {
