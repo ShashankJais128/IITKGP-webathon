76fb9cfb971bb4ceb069f8fe48f4ab7cff1b05bd
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-19 09:50:17 +0530

    Update CreateEvent.jsx

--- src/components/myEvents/CreateEvent.jsx
+++ src/components/myEvents/CreateEvent.jsx
@@ -222,14 +222,14 @@ function CreateEvent({ showModal, setShowModal }) {
                     type="button"
                     onClick={closeModal}
                   >
-                    Close{" "}
+                    Close
                   </button>
                   <button
                     className="bg-[#ff673a] text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={handleClick}
                   >
-                    Save Changes{" "}
+                    Save Changes
                   </button>
                 </div>
               </div>
