bce592658b4539c27eecb78c27c50f4b89dfdc55
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-19 08:14:09 +0530

    Update Details.jsx

--- src/components/myEvents/Details.jsx
+++ src/components/myEvents/Details.jsx
@@ -9,10 +9,12 @@ import DCss from "./Css/Details.module.css";
 export default function Details() {
   const authCtx = useContext(AuthContext);
   return (
-    <div className={DCss.mDiv}>
-      <h1>Details</h1>
-      <p>{authCtx.user.name}</p>
-      <p>{authCtx.user.email}</p>
+    <div className="w-full text-white flex justify-center items-center gap-8">
+      <img src={authCtx.user.pic} alt="" className="rounded-lg" />
+      <div className="flex flex-col gap-8">
+        <p className="text-4xl capitalize">{authCtx.user.name}</p>
+        <p className="text-3xl">{authCtx.user.email}</p>
+      </div>
     </div>
   );
 }
