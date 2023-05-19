566c2f2015a639c03b2385aa001b009900bb26c9
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-19 12:59:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom/client";
+
+// css
+import "./index.css";
+
+// state
 import { AuthContextProvider } from "./store/auth-context";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
