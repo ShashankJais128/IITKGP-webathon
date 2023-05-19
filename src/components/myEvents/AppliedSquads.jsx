431fbbdfd83bd447786d27d15f55423d75415f8e
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-19 14:56:39 +0530

    Update AppliedSquads.jsx

--- src/components/myEvents/AppliedSquads.jsx
+++ src/components/myEvents/AppliedSquads.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 // axios
 import axios from "axios";
@@ -11,28 +11,9 @@ import img from "../../public/Rect.png";
 import peop from "../../public/posted_by.png";
 import linkd from "../../public/linkedin.png";
 
-const appSquad = [
-  {
-    title: "ALPINE TURBINE SQUADS",
-    image: img,
-    status: "Rejected",
-    postby: "Rishav Singh",
-  },
-  {
-    title: "ALPINE TURBINE SQUADS",
-    image: img,
-    status: "Applied",
-    postby: "Shashank",
-  },
-  {
-    title: "ALPINE TURBINE SQUADS",
-    image: img,
-    status: "Accepted",
-    postby: "Aditya",
-  },
-];
-
 function AppliedSquads() {
+  const [showDataMap, setData] = useState([]);
+
   const authCtx = useContext(AuthContext);
 
   useEffect(() => {
@@ -48,50 +29,64 @@ function AppliedSquads() {
       headers: { Authorization: `${authCtx.token}` },
     });
 
-    console.log(resp);
+    if (resp.data) {
+      console.log(resp.data);
+
+      setData(resp.data);
+    }
   };
   return (
     <div className="w-[75%] mx-[5%] mt-20">
       <h1 className="text-white text-4xl font-semibold mb-5">Applied Squads</h1>
 
       <div className="flex flex-col w-full space-y-4">
-        {appSquad.map((data, key) => {
-          return (
-            <div
-              className="bg-[#28282B] flex flex-col items-center md:items-start md:flex-row w-full p-4"
-              key={key}
-            >
-              <div className="">
-                <img src={data.image} alt="" className="w-[180px] h-[180px]" />
-              </div>
-              <div className="px-6 w-full flex flex-col justify-between">
-                <div>
-                  <h1 className="text-3xl text-center md:text-start font-semibold text-[#ff673a]">
-                    {data.title}
-                  </h1>
+        {showDataMap ? (
+          <>
+            {showDataMap.map((data, key) => {
+              return (
+                <div
+                  className="bg-[#28282B] flex flex-col items-center md:items-start md:flex-row w-full p-4"
+                  key={key}
+                >
+                  <div className="">
+                    <img
+                      src={data.competitionID.image}
+                      alt=""
+                      className="w-[180px] h-[180px]"
+                    />
+                  </div>
+                  <div className="px-6 w-full flex flex-col justify-between">
+                    <div>
+                      <h1 className="text-3xl text-center md:text-start font-semibold text-[#ff673a]">
+                        {data.competitionID.name}
+                      </h1>
+                    </div>
+                    <br />
+                    <div className="w-full ">
+                      <p className="text-center md:text-right text-white text-lg">
+                        {data.status}
+                      </p>
+                    </div>
+                    <br />
+                    <div className="flex justify-center md:justify-start pb-4">
+                      <img src={peop} alt="" className="w-6" />
+                      <h1 className="text-white text-xl md:text-start">
+                        &nbsp; Posted by:{" "}
+                        <span className="underline">Rishav Singh</span>
+                        &nbsp; &nbsp;
+                      </h1>
+                      <a href="https://www.linkedin.com" className="w-5">
+                        <img src={linkd} alt="" />
+                      </a>
+                    </div>
+                  </div>
                 </div>
-                <br />
-                <div className="w-full ">
-                  <p className="text-center md:text-right text-white text-lg">
-                    {data.status}
-                  </p>
-                </div>
-                <br />
-                <div className="flex justify-center md:justify-start pb-4">
-                  <img src={peop} alt="" className="w-6" />
-                  <h1 className="text-white text-xl md:text-start">
-                    &nbsp; Posted by:{" "}
-                    <span className="underline">Rishav Singh</span>
-                    &nbsp; &nbsp;
-                  </h1>
-                  <a href="https://www.linkedin.com" className="w-5">
-                    <img src={linkd} alt="" />
-                  </a>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+              );
+            })}
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
