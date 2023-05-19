ed4a29b1c382498a4986574b96a712c23e474dbb
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-19 08:04:21 +0530

    Update Details.jsx

--- src/components/myEvents/Details.jsx
+++ src/components/myEvents/Details.jsx
@@ -3,13 +3,16 @@ import React, { useContext } from "react";
 // redux
 import AuthContext from "../../store/auth-context";
 
+// css
+import DCss from "./Css/Details.module.css";
+
 export default function Details() {
   const authCtx = useContext(AuthContext);
   return (
-    <div>
-      Details
-      <p>authCtx.user.name</p>
-      <p>authCtx.user.email</p>
+    <div className={DCss.mDiv}>
+      <h1>Details</h1>
+      <p>{authCtx.user.name}</p>
+      <p>{authCtx.user.email}</p>
     </div>
   );
 }
