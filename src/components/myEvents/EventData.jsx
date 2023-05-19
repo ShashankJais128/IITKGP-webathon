ad3318374662dbe2964242f413aaf96148a1eb88
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-19 09:50:14 +0530

    Create EventData.jsx

---
+++ src/components/myEvents/EventData.jsx
@@ -0,0 +1,26 @@
+import eveImg from "../../public/aboutimg.png";
+
+export const myEvents = [
+  {
+    title: "Event Name",
+    subtitle: "subtitle",
+    venue: "kgp",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab molestiae eaque rerum reprehenderit in obcaecati architecto quibusdam soluta aliquam, dolores, unde cum iusto error rationesapiente delectus minima tempore nulla placeat doloribus enim essedicta nisi. Reiciendis tempora ex voluptate harum commodi corporisaccusantium repudiandae rerum recusandae, necessitatibus nostrumconsequuntur consectetur, doloremque possimus distinctio errorunde deserunt officiis eaque sit facilis. Similique itaqu necessitatibus tenetur? Maxime quidem quia quaerat officia eaofficiis molestias et itaque!",
+    teamSize: "4",
+    date: "20/3/23",
+    image: eveImg,
+    vacancy: 3,
+  },
+  {
+    title: "Event Name2",
+    subtitle: "subtitle",
+    venue: "kgp",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab molestiae eaque rerum reprehenderit in obcaecati architecto quibusdam soluta aliquam, dolores, unde cum iusto error rationesapiente delectus minima tempore nulla placeat doloribus enim essedicta nisi. Reiciendis tempora ex voluptate harum commodi corporisaccusantium repudiandae rerum recusandae, necessitatibus nostrumconsequuntur consectetur, doloremque possimus distinctio errorunde deserunt officiis eaque sit facilis. Similique itaqu necessitatibus tenetur? Maxime quidem quia quaerat officia eaofficiis molestias et itaque!",
+    teamSize: "4",
+    date: "20/3/23",
+    image: eveImg,
+    vacancy: 2,
+  },
+];
