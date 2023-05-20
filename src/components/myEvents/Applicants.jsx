464243a981d403f13fb0ed234eddcbd1ef1c7e41
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-21 00:41:10 +0530

    Update Applicants.jsx

--- src/components/myEvents/Applicants.jsx
+++ src/components/myEvents/Applicants.jsx
@@ -16,7 +16,7 @@ const skill = ["MongoDB", "React"];
 
 function Applicants() {
   const [showUserInfo, setUserInfo] = useState([]);
-  const [showComInfo, setComInfo] = useState();
+  const [showImg, setImg] = useState();
   let { id } = useParams();
 
   const authCtx = useContext(AuthContext);
@@ -33,9 +33,9 @@ function Applicants() {
 
       if (resp.data) {
         console.log(resp.data);
-        // competitionID
-        setUserInfo(resp.data.comData.userID);
-        setComInfo(resp.data.comData.competitionID);
+
+        setUserInfo(resp.data.comData);
+        setImg(resp.data.comData[0].competitionID.image);
       }
     } catch (e) {
       console.log(e);
@@ -51,91 +51,85 @@ function Applicants() {
   }, [showUserInfo]);
   return (
     <>
-      {showComInfo ? (
-        <>
-          <div className="w-[75%] mx-[5%] mt-16">
-            <img src={showComInfo.image} alt="" />
-            <div className=" bg-[#28282B] p-6">
-              <div className="flex flex-col items-center md:flex-row md:justify-between mb-5">
-                <div>
-                  <select
-                    name=""
-                    id=""
-                    className="p-2 bg-gray-400 text-white text-xl md:text-2xl font-semibold"
-                  >
-                    {showUserInfo ? (
-                      <>
-                        {/* {showUserInfo.map((val, key) => {
-                    return (
-                      <> */}
-                        <option value={showUserInfo.name}>
-                          {showUserInfo.name}
-                        </option>
-                        {/* </>
-                    );
-                  })} */}
-                      </>
-                    ) : (
-                      ""
-                    )}
-                    {/* <option value="">RISHAV SINGH</option>
-              <option value="">SHASHANK JAISWAL</option>
-              <option value="">ADITYA RANJAN</option> */}
-                  </select>
-                </div>
-                <br />
-                <div className="flex space-x-6">
-                  <button className="text-xl text-[#ff673a] font-semibold">
-                    Accept
-                  </button>
-                  <button className="text-xl text-white font-semibold">
-                    Reject
-                  </button>
-                </div>
-              </div>
-              <br />
-              <div className="flex justify-center md:justify-start">
-                <img src={pp} alt="" className="w-6 md:w-8 mr-4" />
-                <h1 className="text-xl sm:text-2xl md:text-3xl text-[#ff673a] font-bold">
-                  Rishav singh
-                </h1>{" "}
-                &nbsp; &nbsp;
-                <a href="https://www.linkedin.com" className="w-6 md:w-8">
-                  <img src={linkd} alt="" />
-                </a>
-              </div>
-              <br />
-              <br />
-              <div className="flex p-2">
-                <div className="w-[50%]">
-                  <h1 className="text-gray-300 text-lg md:text-xl">SKILLS</h1>
-                  <br />
-                  <div className="flex flex-wrap gap-2">
-                    {skill.map((data) => {
+      <div className="w-[75%] mx-[5%] mt-16">
+        <img src={showImg} alt="" />
+        <div className=" bg-[#28282B] p-6">
+          <div className="flex flex-col items-center md:flex-row md:justify-between mb-5">
+            <div>
+              <select
+                name=""
+                id=""
+                className="p-2 bg-gray-400 text-white text-xl md:text-2xl font-semibold"
+              >
+                {showUserInfo ? (
+                  <>
+                    {showUserInfo.map((val, key) => {
                       return (
-                        <p className="p-2 bg-gray-400 w-fit text-white rounded-full">
-                          {data}
-                        </p>
+                        <>
+                          <option value={val.userID.name}>
+                            {val.userID.name}
+                          </option>
+                        </>
                       );
                     })}
-                  </div>
-                </div>
-                <div className="w-[50%]">
-                  <h1 className="text-gray-300 text-lg md:text-xl">MESSAGE</h1>
-                  <br />
-                  <p className="text-white md:text-lg">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Perspiciatis eius vel maxime laborum voluptates quia! Illo
-                    corporis sequi facilis nam!
-                  </p>
-                </div>
-              </div>
+                  </>
+                ) : (
+                  ""
+                )}
+                {/* <option value="">RISHAV SINGH</option>
+              <option value="">SHASHANK JAISWAL</option>
+              <option value="">ADITYA RANJAN</option> */}
+              </select>
+            </div>
+            <br />
+            <div className="flex space-x-6">
+              <button className="text-xl text-[#ff673a] font-semibold">
+                Accept
+              </button>
+              <button className="text-xl text-white font-semibold">
+                Reject
+              </button>
             </div>
           </div>
-        </>
-      ) : (
-        ""
-      )}
+          <br />
+          <div className="flex justify-center md:justify-start">
+            <img src={pp} alt="" className="w-6 md:w-8 mr-4" />
+            <h1 className="text-xl sm:text-2xl md:text-3xl text-[#ff673a] font-bold">
+              Rishav singh
+            </h1>{" "}
+            &nbsp; &nbsp;
+            <a href="https://www.linkedin.com" className="w-6 md:w-8">
+              <img src={linkd} alt="" />
+            </a>
+          </div>
+          <br />
+          <br />
+          <div className="flex p-2">
+            <div className="w-[50%]">
+              <h1 className="text-gray-300 text-lg md:text-xl">SKILLS</h1>
+              <br />
+              <div className="flex flex-wrap gap-2">
+                {skill.map((data) => {
+                  return (
+                    <p className="p-2 bg-gray-400 w-fit text-white rounded-full">
+                      {data}
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="w-[50%]">
+              <h1 className="text-gray-300 text-lg md:text-xl">MESSAGE</h1>
+              <br />
+              <p className="text-white md:text-lg">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Perspiciatis eius vel maxime laborum voluptates quia! Illo
+                corporis sequi facilis nam!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
