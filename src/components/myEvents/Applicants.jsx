a771b30c0af5a3821d99e07d7137120277f1cd48
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-23 08:14:38 +0530

    Update Applicants.jsx

--- src/components/myEvents/Applicants.jsx
+++ src/components/myEvents/Applicants.jsx
@@ -90,36 +90,78 @@ function Applicants() {
 
   return (
     <>
-      <div className="w-[75%] mx-[5%] mt-16">
-        <img src={showImg} alt="" className="w-full" />
-        <div className=" bg-[#28282B] p-6">
-          <div className="flex flex-col items-center md:flex-row md:justify-between mb-5">
-            <div>
-              <select
-                name="userName"
-                id=""
-                className="p-2 bg-gray-400 text-white text-xl md:text-2xl font-semibold"
-                onChange={selectVal}
-              >
-                {showUserInfo ? (
+      {showUserInfo.length > 0 ? (
+        <div className="w-[75%] mx-[5%] mt-16">
+          <img src={showImg} alt="" className="w-full" />
+          <div className=" bg-[#28282B] p-6">
+            <div className="flex flex-col items-center md:flex-row md:justify-between mb-5">
+              <div>
+                <select
+                  name="userName"
+                  id=""
+                  className="p-2 bg-gray-400 text-white text-xl md:text-2xl font-semibold"
+                  onChange={selectVal}
+                >
+                  {showUserInfo ? (
+                    <>
+                      {showUserInfo.map((val, key) => {
+                        return (
+                          <>
+                            <option
+                              value={val.userID.name}
+                              key={val.userID._id}
+                            >
+                              {val.userID.name}
+                            </option>
+                          </>
+                        );
+                      })}
+                    </>
+                  ) : (
+                    ""
+                  )}
+                </select>
+              </div>
+              <br />
+
+              <>
+                {showSelect ? (
                   <>
-                    {showUserInfo.map((val, key) => {
-                      return (
-                        <>
-                          <option value={val.userID.name} key={val.userID._id}>
-                            {val.userID.name}
-                          </option>
-                        </>
-                      );
-                    })}
+                    {showUserInfo ? (
+                      <>
+                        {showUserInfo
+                          .filter((item) => {
+                            return item.userID.name === showSelect;
+                          })
+                          .map((val, key) => {
+                            return (
+                              <div className="flex space-x-6" key={key}>
+                                <button
+                                  className="text-xl text-[#ff673a] font-semibold"
+                                  onClick={(event) => Accept(event, val)}
+                                >
+                                  Accept
+                                </button>
+                                <button
+                                  className="text-xl text-white font-semibold"
+                                  onClick={(event) => Reject(event, val)}
+                                >
+                                  Reject
+                                </button>
+                              </div>
+                            );
+                          })}
+                      </>
+                    ) : (
+                      ""
+                    )}
                   </>
                 ) : (
                   ""
                 )}
-              </select>
+              </>
             </div>
             <br />
-
             <>
               {showSelect ? (
                 <>
@@ -131,20 +173,66 @@ function Applicants() {
                         })
                         .map((val, key) => {
                           return (
-                            <div className="flex space-x-6" key={key}>
-                              <button
-                                className="text-xl text-[#ff673a] font-semibold"
-                                onClick={(event) => Accept(event, val)}
+                            <>
+                              <div
+                                className="flex justify-center md:justify-start"
+                                key={key}
                               >
-                                Accept
-                              </button>
-                              <button
-                                className="text-xl text-white font-semibold"
-                                onClick={(event) => Reject(event, val)}
-                              >
-                                Reject
-                              </button>
-                            </div>
+                                <img
+                                  src={pp}
+                                  alt=""
+                                  className="w-6 md:w-8 mr-4"
+                                />
+                                <h1 className="text-xl sm:text-2xl md:text-3xl text-[#ff673a] font-bold">
+                                  {val.userID.name}
+                                </h1>{" "}
+                                &nbsp; &nbsp;
+                                <a
+                                  href="https://www.linkedin.com"
+                                  className="w-6 md:w-8"
+                                >
+                                  <img src={linkd} alt="" />
+                                </a>
+                              </div>
+                              <br />
+                              <br />
+                              <div className="flex p-2">
+                                {val.userID.skills.length > 0 ? (
+                                  <>
+                                    <div className="w-[50%]">
+                                      <h1 className="text-gray-300 text-lg md:text-xl">
+                                        SKILLS
+                                      </h1>
+                                      <br />
+                                      <div className="flex flex-wrap gap-2">
+                                        {val.userID.skills.map((data, key) => {
+                                          return (
+                                            <p
+                                              className="p-2 bg-gray-400 w-fit text-white rounded-full"
+                                              key={key}
+                                            >
+                                              {data}
+                                            </p>
+                                          );
+                                        })}
+                                      </div>
+                                    </div>
+                                  </>
+                                ) : (
+                                  ""
+                                )}
+
+                                <div className="w-[50%]">
+                                  <h1 className="text-gray-300 text-lg md:text-xl">
+                                    MESSAGE
+                                  </h1>
+                                  <br />
+                                  <p className="text-white md:text-lg">
+                                    {val.message}
+                                  </p>
+                                </div>
+                              </div>
+                            </>
                           );
                         })}
                     </>
@@ -157,91 +245,14 @@ function Applicants() {
               )}
             </>
           </div>
-          <br />
-          <>
-            {showSelect ? (
-              <>
-                {showUserInfo ? (
-                  <>
-                    {showUserInfo
-                      .filter((item) => {
-                        return item.userID.name === showSelect;
-                      })
-                      .map((val, key) => {
-                        return (
-                          <>
-                            <div
-                              className="flex justify-center md:justify-start"
-                              key={key}
-                            >
-                              <img
-                                src={pp}
-                                alt=""
-                                className="w-6 md:w-8 mr-4"
-                              />
-                              <h1 className="text-xl sm:text-2xl md:text-3xl text-[#ff673a] font-bold">
-                                {val.userID.name}
-                              </h1>{" "}
-                              &nbsp; &nbsp;
-                              <a
-                                href="https://www.linkedin.com"
-                                className="w-6 md:w-8"
-                              >
-                                <img src={linkd} alt="" />
-                              </a>
-                            </div>
-                            <br />
-                            <br />
-                            <div className="flex p-2">
-                              {val.userID.skills.length > 0 ? (
-                                <>
-                                  <div className="w-[50%]">
-                                    <h1 className="text-gray-300 text-lg md:text-xl">
-                                      SKILLS
-                                    </h1>
-                                    <br />
-                                    <div className="flex flex-wrap gap-2">
-                                      {val.userID.skills.map((data, key) => {
-                                        return (
-                                          <p
-                                            className="p-2 bg-gray-400 w-fit text-white rounded-full"
-                                            key={key}
-                                          >
-                                            {data}
-                                          </p>
-                                        );
-                                      })}
-                                    </div>
-                                  </div>
-                                </>
-                              ) : (
-                                ""
-                              )}
-
-                              <div className="w-[50%]">
-                                <h1 className="text-gray-300 text-lg md:text-xl">
-                                  MESSAGE
-                                </h1>
-                                <br />
-                                <p className="text-white md:text-lg">
-                                  {val.message}
-                                </p>
-                              </div>
-                            </div>
-                          </>
-                        );
-                      })}
-                  </>
-                ) : (
-                  ""
-                )}
-              </>
-            ) : (
-              ""
-            )}
-          </>
         </div>
-      </div>
+      ) : (
+        <div className="bg-[#28282B] flex flex-col items-center md:items-start md:flex-row w-[80%] p-4 mt-[10%] mx-5 rounded h-fit">
+          <p className="text-[#fff] text-4xl text-center w-full my-5">
+            No Applicants
+          </p>
+        </div>
+      )}
     </>
   );
 }
